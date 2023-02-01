4c394cdf33da98a9374ea545d44d0b18c1f29df8
hynguyen33 <tug63287@temple.edu>
2023-01-31 21:22:44 -0500

    adding layout

--- app/src/main/java/com/example/convoyapp/MainActivity.kt
+++ app/src/main/java/com/example/convoyapp/MainActivity.kt
@@ -2,10 +2,16 @@ package com.example.convoyapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+    lateinit var signIn: Button
+    lateinit var register: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        signIn = findViewById(R.id.logIn)
+        register = findViewById(R.id.register)
     }
 }
